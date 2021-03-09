427168fa010af668121abe655717ee9e66473af5
TeamAggro (~Steve) <teamaggro@aggromc.de>
2021-03-09 16:49:51 +0100

    Create Begger.java

---
+++ src/main/java/dev/teamaggro/dankmemerbot/Begger.java
@@ -0,0 +1,24 @@
+package dev.teamaggro.dankmemerbot;
+
+import java.util.Random;
+
+public class Begger implements Runnable {
+
+    @Override
+    public void run() {
+        final int startDelay = new Random().nextInt(6000);
+        try {
+            Thread.sleep(startDelay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        while (DankMemerBot.getInstance().isRunning()) {
+            try {
+            DankMemerBot.getInstance().textChannel.sendMessage("pls beg").join();
+            Thread.sleep(50000 + new Random().nextInt(1500));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
