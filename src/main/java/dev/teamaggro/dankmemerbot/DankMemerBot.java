11182a7de9e135006c41c72c6e170e535cc88d89
TeamAggro (~Steve) <teamaggro@aggromc.de>
2021-03-10 08:54:33 +0100

    Implement Config

--- src/main/java/dev/teamaggro/dankmemerbot/DankMemerBot.java
+++ src/main/java/dev/teamaggro/dankmemerbot/DankMemerBot.java
@@ -1,5 +1,7 @@
 package dev.teamaggro.dankmemerbot;
 
+import dev.teamaggro.dankmemerbot.commands.*;
+import dev.teamaggro.dankmemerbot.utils.Config;
 import org.javacord.api.AccountType;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
@@ -8,49 +10,64 @@ import org.javacord.api.entity.channel.ServerTextChannel;
 public class DankMemerBot {
 
     private static DankMemerBot INSTANCE;
+    public final DiscordApi discordApi;
+    public final ServerTextChannel textChannel;
+    public final String channelID;
+    public final String autopayID;
+    public boolean isRunning;
+
+    public DankMemerBot(Config config) throws Exception {
+        INSTANCE = this;
+        System.out.println("Logging in with token '" + config.token + "'...");
+        discordApi = new DiscordApiBuilder().setAccountType(AccountType.CLIENT).setToken(config.token).login().join();
+        System.out.println("Logged in as " + discordApi.getYourself().getDiscriminatedName() + "!");
+        textChannel = discordApi.getServerTextChannelById(config.channelid).get();
+        System.out.println("Starting Threads...");
+        isRunning = true;
+        channelID = config.channelid;
+        autopayID = config.autopayid;
+        //They can start instantly
+        if (config.type_check_enabled) {
+            Thread typeThread = new Thread(new TypeListener());
+            typeThread.start();
+        }
+        if (config.beg_enabled) {
+            Thread begThread = new Thread(new Begger());
+            begThread.start();
+        }
+        if (config.hunt_enabled) {
+            Thread huntThread = new Thread(new Hunter());
+            huntThread.start();
+        }
+        if (config.fish_enabled) {
+            Thread fishThread = new Thread(new Fisher());
+            fishThread.start();
+        }
+        if (config.postmeme_enabled) {
+            Thread.sleep(10000);
+            Thread pmThread = new Thread(new MemePoster());
+            pmThread.start();
+        }
+        if (config.trivia_enabled) {
+            Thread.sleep(10000);
+            Thread triviaThread = new Thread(new TriviaGuesser());
+            triviaThread.start();
+        }
+        if (config.autopay_enabled) {
+            Thread.sleep(10000);
+            Thread autoTransfer = new Thread(new AutoSUS());
+            autoTransfer.start();
+        }
+    }
 
     public static DankMemerBot getInstance() {
         return INSTANCE;
     }
 
-    public final DiscordApi discordApi;
-    public final ServerTextChannel textChannel;
-    public boolean isRunning;
-    public String sendToId = "305248730137886722"; // TeamAggro fix this code or i die :flush:
-
-    public DankMemerBot(String token, String channelID) throws Exception {
-        INSTANCE = this;
-        System.out.println("Logging in with token '" + token + "'...");
-        discordApi = new DiscordApiBuilder().setAccountType(AccountType.CLIENT).setToken(token).login().join();
-        System.out.println("Logged in as " + discordApi.getYourself().getDiscriminatedName() + "!");
-        textChannel = discordApi.getServerTextChannelById(channelID).get();
-        System.out.println("Starting Threads...");
-        isRunning = true;
-        //They can start instantly
-        Thread begThread = new Thread(new Begger());
-        begThread.start();
-        Thread huntThread = new Thread(new Hunter());
-        huntThread.start();
-        Thread fishThread = new Thread(new Fisher());
-        fishThread.start();
-        Thread.sleep(10000);
-        Thread pmThread = new Thread(new MemePoster());
-        pmThread.start();
-        Thread.sleep(10000);
-        Thread triviaThread = new Thread(new TriviaGuesser());
-        triviaThread.start();
-        Thread.sleep(10000);
-        Thread autoTransfer = new Thread(new AutoSUS());
-        autoTransfer.start();
-    }
-
     public static void main(String[] args) throws Exception {
-
-        if (args.length != 2) {
-            System.out.println("Usage: java -jar Selfbot.jar TOKEN CHANNELID");
-        }else {
-            new DankMemerBot(args[0], args[1]);
-        }
+        Config conf = new Config();
+        conf.loadProperties();
+        new DankMemerBot(conf);
     }
 
     public boolean isRunning() {
--- src/main/java/dev/teamaggro/dankmemerbot/utils/Config.java
+++ src/main/java/dev/teamaggro/dankmemerbot/utils/Config.java
@@ -1,4 +1,66 @@
 package dev.teamaggro.dankmemerbot.utils;
 
+import java.io.*;
+import java.util.Properties;
+
 public class Config {
+
+    public String token, channelid, autopayid;
+    public boolean beg_enabled, autopay_enabled, fish_enabled, postmeme_enabled, hunt_enabled, trivia_enabled, type_check_enabled;
+
+    public void loadProperties() throws Exception {
+        File configFile = new File("config.properties");
+
+        try {
+            FileReader reader = new FileReader(configFile);
+            Properties props = new Properties();
+            props.load(reader);
+            token = props.getProperty("token");
+            channelid = props.getProperty("channelid");
+            autopayid = props.getProperty("autopay_userid");
+            beg_enabled = Boolean.parseBoolean(props.getProperty("beg_enabled"));
+            fish_enabled = Boolean.parseBoolean(props.getProperty("fish_enabled"));
+            hunt_enabled = Boolean.parseBoolean(props.getProperty("hunt_enabled"));
+            postmeme_enabled = Boolean.parseBoolean(props.getProperty("postmeme_enabled"));
+            trivia_enabled = Boolean.parseBoolean(props.getProperty("trivia_enabled"));
+            autopay_enabled = Boolean.parseBoolean(props.getProperty("autopay_enabled"));
+            type_check_enabled = Boolean.parseBoolean(props.getProperty("type_check_enabled"));
+            reader.close();
+        } catch (FileNotFoundException ex) {
+            createConfig();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void createConfig() throws IOException {
+        File configFile = new File("config.properties");
+        if (!configFile.exists()) {
+            configFile.createNewFile();
+        }
+        try {
+            Properties props = new Properties();
+            props.setProperty("token", "INSERT_CLIENT_TOKEN");
+            props.setProperty("channelid", "818848381798449173");
+            props.setProperty("autopay_userid", "765944420674568224");
+            props.setProperty("autopay_enabled", "true");
+            props.setProperty("fish_enabled", "true");
+            props.setProperty("postmeme_enabled", "true");
+            props.setProperty("trivia_enabled", "true");
+            props.setProperty("hunt_enabled", "true");
+            props.setProperty("beg_enabled", "true");
+            props.setProperty("type_check_enabled", "true");
+            FileWriter writer = new FileWriter(configFile);
+            props.store(writer, "DankMemer Selfbot Settings");
+            writer.close();
+            loadProperties();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
